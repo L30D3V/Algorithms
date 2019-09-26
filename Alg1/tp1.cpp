4304178b2b1165f1499a2b027546e6bc85cb6e2b
Leonardo Dias <leo.thdias@gmail.com>
2019-09-26 14:57:18 -0300

    Started SWAP

--- Alg1/tp1.cpp
+++ Alg1/tp1.cpp
@@ -8,68 +8,11 @@ std::vector<bool> visited;
 std::vector<int> position_to_age;
 int younger;
 
-int depth_first_search(int a, std::vector<std::vector<int> > graph) {
-    // std::cout << "[DFS - Start] - Depth search for " << a << std::endl;
+int commander(int a, std::vector<std::vector<int> > graph);
+bool swap(int a, int b, std::vector<std::vector<int> > graph);
 
-    visited[a-1] = true;
-
-    // std::cout << "[DFS - Visited] - ";
-    // for (int i = 0; i < visited.size(); i++) {
-    //     std::cout << visited[i] << " ";
-    // }
-    // std::cout << std::endl;
-
-    if (position_to_age[a-1] < younger || younger == -1) {
-        younger = position_to_age[a-1];
-    }
-
-    // if (graph[a-1].empty()) {
-    //     std::cout << "[DFS - Empty]" << std::endl;
-    // }
-
-    // for (int i = 0; i < graph[a-1].size(); i++) {
-    //     std::cout << graph[a-1][i] << " ";
-    // }
-    // std::cout << std::endl;
-
-    for (int i = 0; i < graph[a-1].size(); i++) {
-        if (!visited[graph[a-1][i] - 1]) {
-            int result_search = depth_first_search(graph[a-1][i], graph);
-
-            // std::cout << "[DFS - " << a << "] - Retorno: " << result_search << std::endl;
-            if (result_search < younger || younger == -1) {
-                younger = result_search;
-            }
-        }
-        // else
-            // std::cout << graph[a-1][i] << " visited already" << std::endl;
-    }
-
-    // std::cout << "[DFS - End] - " << a << " " << younger << std::endl;
-
-    return younger;
-}
-
-int commander(int a, std::vector<std::vector<int> > graph) {
-    // std::cout << std::endl;
-    // std::cout << "-----------------------------------" << std::endl;
-    // std::cout << "[COMMANDER] - START" << std::endl;
-
-    // Destroy current values and resize vector
-    visited.resize(0);
-    visited.resize(graph.size());
-
-    // Initialize younger age
-    younger = -1;
-    int result = depth_first_search(a, graph);
-    if (result == position_to_age[a-1])
-        return -1;
-    else
-        return result;
-
-    // std::cout << "[COMMANDER] - END" << std::endl;
-    // std::cout << "-----------------------------------" << std::endl;
-}
+int search_younger_age(int a, std::vector<std::vector<int> > graph);
+bool verify_cycle(std::vector<std::vector<int> > graph);
 
 int main(int argc, char* argv[]) {
     int n, m, i, read_temp, x, y;
@@ -162,15 +105,11 @@ int main(int argc, char* argv[]) {
                 std::cout << "A - ";
                 std::cout << a << " " << b << std::endl;
 
-                // Verifica se A é hierarquicamente superior a B
-                if (graph[a-1][b-1] != 0) {
-                    // Verificar se a troca cria um ciclo
-                    // Tomar B como ponto de partida
-
-                } else {
+                if (swap(a, b, graph))
+                    std::cout << "S T" << std::endl;
+                else
                     std::cout << "S N" << std::endl;
-                }
-
+                
                 break;
             case 'M':
                 // Ordem de fala na reunião.
@@ -187,4 +126,97 @@ int main(int argc, char* argv[]) {
     dataset.close();
 
     return 0;
+}
+
+int search_younger_age(int a, std::vector<std::vector<int> > graph) {
+    // std::cout << "[DFS - Start] - Depth search for " << a << std::endl;
+
+    visited[a-1] = true;
+
+    // std::cout << "[DFS - Visited] - ";
+    // for (int i = 0; i < visited.size(); i++) {
+    //     std::cout << visited[i] << " ";
+    // }
+    // std::cout << std::endl;
+
+    if (position_to_age[a-1] < younger || younger == -1) {
+        younger = position_to_age[a-1];
+    }
+
+    // if (graph[a-1].empty()) {
+    //     std::cout << "[DFS - Empty]" << std::endl;
+    // }
+
+    // for (int i = 0; i < graph[a-1].size(); i++) {
+    //     std::cout << graph[a-1][i] << " ";
+    // }
+    // std::cout << std::endl;
+
+    for (int i = 0; i < graph[a-1].size(); i++) {
+        if (!visited[graph[a-1][i] - 1]) {
+            int result_search = search_younger_age(graph[a-1][i], graph);
+
+            // std::cout << "[DFS - " << a << "] - Retorno: " << result_search << std::endl;
+            if (result_search < younger || younger == -1) {
+                younger = result_search;
+            }
+        }
+        // else
+            // std::cout << graph[a-1][i] << " visited already" << std::endl;
+    }
+
+    // std::cout << "[DFS - End] - " << a << " " << younger << std::endl;
+
+    return younger;
+}
+
+int commander(int a, std::vector<std::vector<int> > graph) {
+    // std::cout << std::endl;
+    // std::cout << "-----------------------------------" << std::endl;
+    // std::cout << "[COMMANDER] - START" << std::endl;
+
+    // Destroy current values and resize vector
+    visited.resize(0);
+    visited.resize(graph.size());
+
+    // Initialize younger age
+    younger = -1;
+    int result = search_younger_age(a, graph);
+    if (result == position_to_age[a-1])
+        return -1;
+    else
+        return result;
+
+    // std::cout << "[COMMANDER] - END" << std::endl;
+    // std::cout << "-----------------------------------" << std::endl;
+}
+
+bool swap(int a, int b, std::vector<std::vector<int> > graph) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "[SWAP] - START" << std::endl;
+
+    bool has_edge = false;
+
+    for (int i = 0; i < graph[b-1].size(); i++) {
+        if (graph[b-1][i] == a)
+            has_edge = true;
+    }
+
+    if (has_edge) {
+        // TODO: creates new graph as a copy of the original
+        // TODO: remove item from graph[b-1]
+        // TODO: add edge to graph[a-1]
+
+        verify_cycle(graph);
+    } else {
+        return false;
+    }
+
+
+    std::cout << "[SWAP] - END" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+}
+
+bool verify_cycle(std::vector<std::vector<int> > graph) {
+    return false;
 }
