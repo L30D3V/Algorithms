21fa6d0cbdf3da2414b5e32140212ce591bd386a
Leonardo Dias <leo.thdias@gmail.com>
2019-09-26 12:04:25 -0300

    Cpp DLL Tests

---
+++ CppLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CppBind
+{
+    class Program
+    {
+        [DllImport(@"cpp/")]
+        public static extern void writePNGData();        
+        
+        static void Main(string[] args)
+        {
+            writePNGData();
+        }
+    }
+}
