1c0891ed4a03ff38d19ba045061a8202fa4596f3
Leonardo Dias <leo.thdias@gmail.com>
2019-09-30 14:30:42 -0300

    Working DLL consumption on C#

---
+++ DLL/CSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DLL
+{
+    class Program
+    {
+        [DllImport("dll/DllTest.dll")]
+        private static extern void fibonacci_init(ulong a, ulong b);
+        [DllImport("dll/DllTest.dll")]
+        private static extern bool fibonacci_next();
+        [DllImport("dll/DllTest.dll")]
+        private static extern ulong fibonacci_current();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Fibonacci Sequence ----");
+
+            fibonacci_init(1, 1);
+
+            for(int i = 0; i < 100; i++) {    
+                fibonacci_next();
+
+                Console.Write(fibonacci_current() + " ");
+            }
+        }
+    }
+}
